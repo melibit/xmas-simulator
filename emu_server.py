fa5e1adbead46df69141256cb2d31af7f7de2afe
Meli <melikije@outlook.com>
2025-12-13 14:46:37 +0000

    ?

--- emu_server.py
+++ emu_server.py
@@ -87,6 +87,7 @@ HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 5678               # Arbitrary non-privileged port
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
         s.listen(1)
         conn, addr = s.accept()
